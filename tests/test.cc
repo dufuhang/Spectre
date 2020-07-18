28f2b2a46355a56e51d14d0058bdecafc5a744c7
dufuhang <286046056@qq.com>
2020-07-18 15:40:03 +0800

    修改main函数

--- tests/test.cc
+++ tests/test.cc
@@ -7,6 +7,13 @@ int main(int argc, char** argv)
     spectre::Logger::ptr logger(new spectre::Logger);
     logger -> addAppender(spectre::LogAppender::ptr(new spectre::StdoutLogAppender));
 
+    //这里写不进去
+    spectre::FileLogAppender::ptr file_appender(new spectre::FileLogAppender("./log.txt"));
+    spectre::LogFormatter::ptr fmt(new spectre::LogFormatter("%d%T%m%n"));
+    file_appender -> setFormatter(fmt);
+    file_appender -> setLevel(spectre::LogLevel::ERROR);
+
+    logger -> addAppender(file_appender);
     //spectre::LogEvent::ptr event(new spectre::LogEvent(__FILE__, __LINE__, 0, spectre::GetThreadId(), spectre::GetFiberId(), time(0)));
     //event -> getSS() << "hello spectre!";
     //logger -> log(spectre::LogLevel::DEBUG, event);
@@ -15,5 +22,9 @@ int main(int argc, char** argv)
 
     SPECTRE_LOG_INFO(logger) << "test marco!";
     SPECTRE_LOG_ERROR(logger) << "test marco error";
+
+    SPECTRE_LOG_FMT_ERROR(logger, "test marco fmt error %s", "aa");
+    auto l = spectre::Loggermgr::GetInstance() -> getLogger("xx");
+    SPECTRE_LOG_INFO(l) << "xx";
     return 0;
 }
